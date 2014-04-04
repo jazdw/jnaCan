12b17a33c2d1e3210c4500eb5747f8b35c4f30a7
Jared Wiltshire <jazdw@server.fake>
2014-04-04 16:44:44 +1100

    Potential fix

--- core/src/net/jazdw/jnacan/BcmCanSocket.java
+++ core/src/net/jazdw/jnacan/BcmCanSocket.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.net.SocketException;
 
 import net.jazdw.jnacan.c.bcm_msg_head;
+import net.jazdw.jnacan.c.can_frame;
 
 /**
  * Copyright (C) 2014 Jared Wiltshire. All rights reserved.
@@ -32,6 +33,7 @@ public class BcmCanSocket extends CanSocket<bcm_msg_head, BcmMessage> {
     public BcmMessage receive() throws IOException {
         bcm_msg_head msg = new bcm_msg_head();
         super.receive(msg);
+        readFrames(msg);
         return new BcmMessage(msg);
     }
 
@@ -42,6 +44,23 @@ public class BcmCanSocket extends CanSocket<bcm_msg_head, BcmMessage> {
     public BcmMessage receiveFrom(CanInterface canIf) throws IOException {
         bcm_msg_head msg = new bcm_msg_head();
         super.receiveFrom(msg, canIf);
+        readFrames(msg);
         return new BcmMessage(msg);
+    }
+    
+    /**
+     * When doing read, bcm_msg_head.frames will be 0 length
+     * so the first read() call will not fill the array.
+     * This will set the array to the correct length and do the read again.
+     * 
+     * Not sure if this is actually necessary, will nframes ever be > 0? 
+     * 
+     * @param msg
+     */
+    private void readFrames(bcm_msg_head msg) {
+        if (msg.nframes > 0) {
+            msg.frames = new can_frame[msg.nframes];
+            msg.read();
+        }
     }
 }
